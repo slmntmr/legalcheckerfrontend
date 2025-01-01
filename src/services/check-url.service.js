cc8ca53e2fd49d061e0740bbbefb1f485504001f
Süleyman TİMUR <slmn1905tmr@gmail.com>
2025-01-01 23:19:41 +0300

    vs

--- src/services/check-url.service.js
+++ src/services/check-url.service.js
@@ -1,10 +1,10 @@
-const API_URL = process.env.BASE_URL || "http://localhost:8080"; // Backend URL'si dinamik olarak alınır
+const API_URL = "https://legalcheckerbackend.onrender.com"; // Canlı backend URL'si
 
 import axios from "axios";
 
 export const checkUrl = async (url) => {
   try {
-    const response = await axios.post(`${API_URL}/api/check-url`, { url }); // API_URL burada kullanılıyor
+    const response = await axios.post(`${API_URL}/api/check-url`, { url });
     return response.data;
   } catch (error) {
     console.error("Axios error:", error);
