faa6cf192120b4611501cc50fae50e41ac84156b
Süleyman TİMUR <slmn1905tmr@gmail.com>
2025-01-01 20:56:20 +0300

    v5

--- src/services/check-url.service.js
+++ src/services/check-url.service.js
@@ -1,8 +1,10 @@
+const API_URL = process.env.BASE_URL || "http://localhost:8080"; // Backend URL'si dinamik olarak alınır
+
 import axios from "axios";
 
 export const checkUrl = async (url) => {
   try {
-    const response = await axios.post("/api/check-url", { url });
+    const response = await axios.post(`${API_URL}/api/check-url`, { url }); // API_URL burada kullanılıyor
     return response.data;
   } catch (error) {
     console.error("Axios error:", error);
